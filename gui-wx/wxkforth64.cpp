63fe1d1b53342f8bf29db3dc08635abb1985415a
Serge Balakshiy <serge.balakshiy@gmail.com>
2021-01-21 23:38:16 +0300

    kforth-64 work

--- gui-wx/wxkforth64.cpp
+++ gui-wx/wxkforth64.cpp
@@ -60,7 +60,7 @@ using namespace std;
 //#include <math.h>
 //#include <ctype.h>
 //#include <stdio.h>
-#define KF64_BNC  wxConvUTF8
+#define KF64FORTH_ENC  wxConvUTF8
 extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
@@ -72,6 +72,7 @@ extern "C" {
 #include "ForthCompiler.h"
 #include "ForthVM.h"
 #include "VMerrors.h"
+#include "kfmacros.h"
 
 extern vector<WordList> Dictionary;
 extern "C" long int* JumpTable;
@@ -91,6 +92,21 @@ int debug = 0;
 #define kforth64_info(...)    do { } while(0)
 #define kforth64_debug(...)   do { } while(0)
 
+extern "C" long int* GlobalSp;
+#define POP (++GlobalSp)
+extern "C" long int* BottomOfStack;
+extern "C" byte* GlobalTp;
+
+using Cell = uintptr_t;
+using Char = unsigned char;
+using CAddr = Char*;
+
+#define CELL(x)reinterpret_cast<Cell>(x)
+#define CADDR(x) reinterpret_cast<Char*>(x)
+
+extern vector<char*> StringTable;
+extern "C" char* pTIB;
+extern vector<byte>* pCurrentOps;
 
 class AbortException: public runtime_error {
 public:
@@ -102,48 +118,187 @@ void kf64_abort(){
 throw AbortException("");
 }
 
+int kf64CheckEvents(){
+  if (allowcheck) {
+    wxGetApp().Poller()->checkevents();
+  }
+  if (insideYield) return 0;
+  if (isaborted){
+    //cout << "kf64:checkexents error" << endl;
+    return -1;
+  }
+}
+
 extern "C" int CPP_gprivet(){
-cout << "Privet from Golly!" << endl;
+cout << "CPP_gprivet: Privet from Golly!" << endl;
 return 0;
 }
 
 extern "C" int CPP_gllshow(){
+  kf64CheckEvents();
+  if (GlobalSp > BottomOfStack){
+    return E_V_STK_UNDERFLOW;
+  }
+  auto length = *((int*)(POP));
+  auto caddr = (char*)*(GlobalSp + 1); //TOP  
+  DROP
+    bool get = true;
+
   inscript = false;
-  statusptr->DisplayMessage( wxString( "Privet from kForth64!", KF64_BNC));
+//  statusptr->DisplayMessage( wxString( "Privet from kForth64!", KF64FORTH_ENC));
+  statusptr->DisplayMessage( wxString( caddr, KF64FORTH_ENC));
   inscript = true;
   if (!showstatus) mainptr->ToggleStatusBar();
   return 0;
 }
 
-extern "C" int kf64CheckEvents(){
-  if (allowcheck) wxGetApp().Poller()->checkevents();
-  if (insideYield) return 0;
-  if (isaborted){
-    cout << "kf64:checkexents error" << endl;
-  }
-}
-
-extern "C" int kf64_rotate(){
+extern "C" int CPP_G_kf64rotate(){
+  cout << "CPP_G_kf64rotate: Rotate from Golly!" << endl;
   kf64CheckEvents();
   int direction = 1;
+  cout << "CPP_G_kf64rotate: step 01" << endl;
   if (viewptr->SelectionExists()){
+    cout << "CPP_G_kf64rotate: step 02" << endl;
     viewptr->RotateSelection(direction == 0);
+    cout << "CPP_G_kf64rotate: step 03" << endl;
     DoAutoUpdate();
+    cout << "CPP_G_kf64rotate: step 04" << endl;
+    return 0;
+  }
+  cout << "CPP_G_kf64rotate: step 05" << endl;
+  throw AbortException("not selected");
+  //return 0;
+}
+
+extern "C" void CPP_G_kf64update(){
+  kf64CheckEvents();
+  GSF_update();
+
+
+}
+
+extern "C" int CPP_G_kf64getcell(){ // ( y x -- n = state )
+  kf64CheckEvents();
+  cout << "GlobalSp = " << GlobalSp << endl;
+  //int x = *((int*)(++GlobalSp));
+  int y = *((int*)(POP));
+  cout << "POP GlobalSp = " << GlobalSp << endl;
+  //int y = *((int*)(++GlobalSp));
+  int x = *((int*)(POP));
+  cout << "x = " << x << ", y = " << y << ", POP GlobalSp = " << GlobalSp << endl;
+  const char* err = GSF_checkpos(currlayer->algo, x, y);
+  if (err){
+    throw AbortException("getcell: error: checkpos.");
+  }else{
+    *GlobalSp = currlayer->algo->getcell(x,y) ; // PUSH
+    --GlobalSp;
+    cout << "Push GlobalSp = " << GlobalSp << endl;
+  }
+
+  return 0;
+}
+
+extern "C" int CPP_G_kf64setcell(){
+  kf64CheckEvents();
+  const char* err;
+  auto setcell_state = *((int*)(POP));
+  auto y = *((int*)(POP));
+  auto x = *((int*)(POP));
+  err = GSF_setcell( x, y, setcell_state);
+  if (err == NULL){
     return 0;
   }else{
-    //return -1;
-    throw AbortException("not selected");
+    throw AbortException( err );
   }
 }
 
-//extern "C" int gop_show(){
-//cout << "show from kForth64!" << endl;
-//}
 
-//WordTemplate GollyWords[] = {
-//			     { "PRIVET", gop_privet, 0 },
-//			     { "SHOW", gop_show, 0 },
-//};
+extern "C" int CPP_G_kf64getcursor(){
+  kf64CheckEvents();
+  string s = CursorToString(currlayer->curs);
+  long int ptr = *(GlobalSp + 1); //TOP  
+  char* pstr = (char*)ptr;
+  int nc = s.length();
+  cout << "str =" << ptr << endl;
+  pstr[0] = nc; pstr++;
+  strncpy(pstr, s.c_str(), nc);
+  pstr[nc+2] = '\0';
+  cout << "str =" << ptr << ", cursor=" << pstr << endl;
+  return 0;
+}
+
+extern "C" int CPP_G_kf64getevent(){ //( c-addr u1 -- c-addr u2 )
+  kf64CheckEvents();
+if (GlobalSp > BottomOfStack){
+return E_V_STK_UNDERFLOW;
+}
+auto length = *((int*)(POP));
+auto caddr = (char*)*(GlobalSp + 1); //TOP  
+bool get = true;
+wxString event;
+string ss;
+GSF_getevent(event, get ? 1 : 0);
+// 
+inscript = false;
+statusptr->DisplayMessage(event);
+inscript = true;
+if (!showstatus) mainptr->ToggleStatusBar();
+//
+ss = (const char*)event.mb_str(KF64FORTH_ENC);
+
+const char* cstr = event.mb_str(KF64FORTH_ENC);
+//cout << "wxstr event:" << event.mb_str(KF64FORTH_ENC) << ", wxstr length " << event.length() << endl;
+if (ss.length() > 0){
+cout << "ss event:" << (char*)ss.c_str() << ", length " << ss.length() << endl;
+cout << "cstr event:" << cstr << ", length " << endl;
+}
+strcpy((char*)caddr, cstr); //ss.data());
+    *GlobalSp = (long int)event.length() ; // PUSH
+    --GlobalSp;
+return 0;
+}
+
+extern "C" int CPP_G_kf64getxy_01(){
+  kf64CheckEvents();
+if (GlobalSp > BottomOfStack){
+return E_V_STK_UNDERFLOW;
+}
+
+long int n = TOS;
+// надо разбираться с TOS
+byte *p = new (nothrow) byte[48];
+PUSH_ADDR( (long int) p )
+
+return 0;
+}
+
+extern "C" int CPP_G_kf64getxy(){
+  kf64CheckEvents();
+  if (GlobalSp > BottomOfStack){
+    return E_V_STK_UNDERFLOW;
+  }
+  auto length = *((int*)(POP));
+  auto caddr = (char*)*(GlobalSp + 1); //TOP  
+  bool get = true;
+
+  wxString event;  //event;
+  string ss;
+
+  statusptr->CheckMouseLocation( mainptr->infront); // sets mousepos
+
+  if ( viewptr->showcontrols ) mousepos = wxEmptyString;
+
+  //cout << "mousepos " << mousepos.data() << endl;
+  ss =  (const char*)mousepos.mb_str(KF64FORTH_ENC);
+  //cout << "getxy:" << ss.data() << ", length " << ss.length() << endl;
+
+  const char* cstr = mousepos.mb_str(KF64FORTH_ENC);
+  strcpy((char*)caddr, cstr); //ss.data());
+  *GlobalSp = (long int)mousepos.length() ; // PUSH
+  --GlobalSp;
+//PUSH_IVAL((long int)mousepos.length())  
+return 0;
+}
 
 void RunkForth64Script(const wxString& filepath){
   char t[132];
@@ -173,46 +328,45 @@ void RunkForth64Script(const wxString& filepath){
   const char* prompt = " ok\n";
   set_start_mem();
   int nWords = OpenForth();
-
+  try{
     if (argc0 < 2) {
 	cout << "kForth-64 v " << version << "\t (Build: " << build << ")" << endl;
 	cout << "Copyright (c) 1998--2020 Krishna Myneni" << endl;
         cout << "Contributions by: dpw gd mu bk abs tn cmb bg dnw imss" << endl;
 	cout << "Provided under the GNU Affero General Public License, v3.0 or later" 
 	  << endl << endl;
-    }
-    else {
-    	int i = 1;
-
-	while (i < argc0) {
-	  if (!strcmp(argv0[i], "-D")) {
-	    debug = -1;
-	  }
-	  else if (!strcmp(argv0[i], "-e")) {
-	    if (argc0 > i) {
-	      initial_commands << argv0[i+1] << endl; 
-	    }
-	    ++i;
-	  }
-	  else {
-	    initial_commands << "include " << argv0[i] << endl;
+    }else {
+      int i = 1;
+      
+      while (i < argc0) {
+	if (!strcmp(argv0[i], "-D")) {
+	  debug = -1;
+	}
+	else if (!strcmp(argv0[i], "-e")) {
+	  if (argc0 > i) {
+	    initial_commands << argv0[i+1] << endl; 
 	  }
 	  ++i;
-        }
-        pSS = new istringstream(initial_commands.str());
+	}
+	else {
+	  initial_commands << "include " << argv0[i] << endl;
+	}
+	++i;
+      }
+      pSS = new istringstream(initial_commands.str());
     }
 
     if (debug) {
-	cout << '\n' << nWords << " words defined.\n";
-	cout << "Jump Table address:  " << &JumpTable << endl;
-	cout << "Bottom of Stack:     " << BottomOfStack << endl;
-	cout << "Bottom of Ret Stack: " << BottomOfReturnStack << endl;
+      cout << '\n' << nWords << " words defined.\n";
+      cout << "Jump Table address:  " << &JumpTable << endl;
+      cout << "Bottom of Stack:     " << BottomOfStack << endl;
+      cout << "Bottom of Ret Stack: " << BottomOfReturnStack << endl;
     }
 
     SetForthOutputStream (cout);
     long int line_num = 0, ec = 0;
     vector<byte> op;
-
+    
     if (pSS) {
       SetForthInputStream(*pSS);
       ec = ForthCompiler (&op, &line_num);
@@ -222,13 +376,17 @@ void RunkForth64Script(const wxString& filepath){
       delete pSS; pSS = NULL;
       op.erase(op.begin(), op.end());
       cout << prompt ;
+    }else{
+      cout << "\nReady!\n";
     }
-    else
-        cout << "\nReady!\n";
+  }
+  catch(AbortException){
+    std::cerr << "main caught int exception\n";
 
+  }
 //----------------  the interpreter main loop
 
-ClearControlStacks();
+  ClearControlStacks();
 
 /***
 // Дальнейшее отключаю, так как нужно только обработать
